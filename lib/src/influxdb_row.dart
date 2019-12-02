8ecaf6c92370fb670793216d812d88fda6f2f89d
Rick Spencer <richard.linger.spencer.3@gmail.com>
2019-12-02 20:49:17 +0000

    create influxdb_query/row/table and used it in chart

---
+++ lib/src/influxdb_row.dart
@@ -0,0 +1,43 @@
+import 'dart:collection';
+
+import 'package:flutter/foundation.dart';
+
+class InfluxDBRow extends ListBase<dynamic> {
+  List<dynamic> _fields;
+  List<String> keys;
+
+  InfluxDBRow.fromList(
+      {@required List<dynamic> fields, @required List<String> keys}) {
+    _fields = fields;
+    this.keys = keys;
+  }
+
+  dynamic get value {
+    return _fields[keys.indexOf("_value")];
+  }
+
+  double get measurement {
+    return double.parse(_fields[keys.indexOf("_measurement")].toString());
+  }
+
+  int get millisecondsSinceEpoch {
+    return DateTime.parse(utcString).millisecondsSinceEpoch;
+  }
+
+  String get utcString {
+    return _fields[keys.indexOf("_time")].toString();
+  }
+
+  @override
+  int length;
+
+  @override
+  operator [](int index) {
+    return _fields[index];
+  }
+
+  @override
+  void operator []=(int index, value) {
+    _fields[index] = value;
+  }
+}
---
+++ lib/src/influxdb_table.dart
@@ -0,0 +1,22 @@
+import 'package:csv/csv.dart';
+import 'package:flux_mobile/src/influxdb_row.dart';
+
+class InfluxDBTable {
+  List<String> keys;
+  List<InfluxDBRow> rows = [];
+
+  InfluxDBTable.fromCSV(String csv) {
+    CsvToListConverter converter = CsvToListConverter();
+    List<List<dynamic>> rowObjs = converter.convert(csv);
+
+    keys = List<String>.from(rowObjs[0]);
+
+    // delete the row of fieldnames
+    rowObjs.removeAt(0);
+
+    // create the rows
+    rowObjs.forEach((List<dynamic> row){
+        rows.add(InfluxDBRow.fromList(fields:row, keys:keys));
+    });
+  }
+}
