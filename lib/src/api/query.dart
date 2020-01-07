4519ea1f191b7b5a00a64788f64294af6a877b51
Wojciech Kocjan <wojciech.kocjan@gmail.com>
2020-01-07 15:17:32 +0100

    chore(lib/src): add documentation for subset of APIs

--- lib/src/api/query.dart
+++ lib/src/api/query.dart
@@ -3,14 +3,19 @@ import 'package:flutter/foundation.dart';
 import '../api/api.dart';
 import './table.dart';
 
+/// Class for executing InfluxDB query and retrieving the result, returning it as [List] of [InfluxDBTable] objects.
 class InfluxDBQuery {
+  /// Instance of [InfluxDBAPI] object for running the InfluxDB API calls.
   final InfluxDBAPI api;
+
+  /// Query string to run.
   final String queryString;
+  /// Tables with the result, only available after `execute` has been called.
   List<InfluxDBTable> tables = [];
-  String errorString;
 
   InfluxDBQuery({@required this.api, @required this.queryString});
 
+  /// Executes the query and returns a [Future] to [List] of [InfluxDBTable] objects.
   Future<List<InfluxDBTable>> execute() async {
     // 1: Make the rest call
     String body = await api.postFluxQuery(queryString);
