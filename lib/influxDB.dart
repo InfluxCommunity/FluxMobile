7fdd80583b43ef7d251d2866c1261b72527e4f7c
Wojciech Kocjan <wojciech.kocjan@gmail.com>
2020-01-07 15:46:19 +0100

    feat: enable dartdoc documentation generation

---
+++ lib/influxDB.dart
@@ -0,0 +1,9 @@
+library flux_mobile;
+
+export './src/api/api.dart';
+export './src/api/dashboard.dart';
+export './src/api/error.dart';
+export './src/api/query.dart';
+export './src/api/table.dart';
+export './src/ui/color_scheme.dart';
+export './src/ui/line_chart_widget.dart';
--- lib/src/influxDB.dart
+++
@@ -1,7 +0,0 @@
-export 'api/api.dart';
-export 'api/dashboard.dart';
-export 'api/error.dart';
-export 'api/query.dart';
-export 'api/table.dart';
-export 'ui/color_scheme.dart';
-export 'ui/line_chart_widget.dart';
